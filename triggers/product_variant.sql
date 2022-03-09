fcc067d910f70f0197b17a5e1b9f582a02fbfb95
Sergio Peña <sergiopena@MacBook-Air-de-Sergio.local>
2022-03-09 10:18:10 -0500

    Create table product_audit

---
+++ triggers/product_variant.sql
@@ -0,0 +1,14 @@
+CREATE TABLE marketplace.product_audit(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    size VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    product_category VARCHAR(255) NOT NULL,
+    event_type VARCHAR(255) NOT NULL,
+    change_datetime TIMESTAMP DEFAULT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
